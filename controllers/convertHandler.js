6db0ca4feae0af0d384ad7f9ecad2fc3fc3695d4
Replit user <>
2023-09-26 17:54:10 +0800

    first commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,140 @@
+/*seperate numbers and letters*/
+var numLettersSplitRegex = /[a-z]+|[^a-z]+/gi
+/*check for fraction regex*/
+
+
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
+    var result;
+
+    result = input.match(numLettersSplitRegex)[0]
+
+    let numberRegex = /\d/
+
+    if (numberRegex.test(result) === false) {
+      result = 1
+    }
+
+    if (result.toString().includes('/')) {
+      let values = result.toString().split('/')
+      if (values.length != 2) {
+        return NaN
+      }
+      values[0] = parseFloat(values[0])
+      values[1] = parseFloat(values[1])
+      result = parseFloat((values[0] / values[1]).toFixed(5))
+    }
+
+    if (isNaN(result)) {
+      return NaN
+    }
+
+    return parseFloat(result);
+  }
+
   this.getUnit = function(input) {
     let result;
-    
-    return result;
+
+
+    let len = input.match(numLettersSplitRegex).length
+
+    if (len == 1)
+      result = input.match(numLettersSplitRegex)[0]
+    if (len == 2)
+      result = input.match(numLettersSplitRegex)[1]
+
+    result = result.toLowerCase()
+    if (result.toUpperCase() === 'L')
+      return result = 'L'
+
+    return result
   };
-  
+
+
   this.getReturnUnit = function(initUnit) {
     let result;
-    
-    return result;
+
+    if (initUnit.toUpperCase() === 'GAL')
+      return result = 'L';
+    else if (initUnit.toUpperCase() === 'L')
+      return result = 'gal'
+
+    if (initUnit.toUpperCase() === 'KM')
+      return result = 'mi';
+    else if (initUnit.toUpperCase() === 'MI')
+      return result = 'km'
+
+    if (initUnit.toUpperCase() === 'LBS')
+      return result = 'kg'
+    else if (initUnit.toUpperCase() === 'KG')
+      return result = 'lbs';
+
+    return undefined;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
-    return result;
+
+    return result
   };
-  
+
+
+
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+
     let result;
-    
-    return result;
-  };
-  
+
+
+
+    if (initUnit.toUpperCase() === 'GAL')
+      result = (initNum * galToL).toFixed(5);
+    else if (initUnit.toUpperCase() === 'L')
+      result = (initNum / galToL).toFixed(5);
+
+    if (initUnit.toUpperCase() === 'LBS')
+      result = (initNum * lbsToKg).toFixed(5);
+    else if (initUnit.toUpperCase() === 'KG')
+      result = (initNum / lbsToKg).toFixed(5);
+
+    if (initUnit.toUpperCase() === 'MI')
+      result = (initNum * miToKm).toFixed(5);
+    else if (initUnit.toUpperCase() === 'KM')
+      result = (initNum / miToKm).toFixed(5);
+
+    return result = parseFloat(result)
+
+  }
+
+  let translateObj = {
+    KM: 'kilometers',
+    GAL: 'gallons',
+    MI: 'miles',
+    KG: 'kilograms',
+    LBS: 'pounds',
+    L: 'liters'
+  }
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+    let initUnitBig = initUnit.toUpperCase()
+    let returnUnitBig = returnUnit.toUpperCase()
+    console.log({ initUnitBig })
+
+    if (translateObj.hasOwnProperty(initUnitBig)) {
+      let translatedInitUnit = translateObj[initUnitBig];
+      let translatedReturnUnit = translateObj[returnUnitBig];
+      console.log(translatedInitUnit)
+      result = `${initNum} ${translatedInitUnit} converts to ${returnNum} ${translatedReturnUnit}`
+    }
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,44 @@
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let convertHandler = new ConvertHandler();
 
+
+  app.route('/api/convert')
+    .get(function(req, res) {
+      var input = req.query.input;  
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      var returnNum = parseFloat(convertHandler.convert(initNum, initUnit));
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+
+
+      if((isNaN(initNum) || initNum <= 0) && returnUnit != undefined)
+        res.json('invalid number')
+      else if(returnUnit == undefined && !(isNaN(initNum) || initNum <= 0))
+        res.json('invalid unit')
+      else if(returnUnit == undefined && (isNaN(initNum) || initNum <= 0))
+        res.json('invalid number and unit')
+      
+      
+
+      
+      var string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+      let resObj = {
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: string,
+      }
+
+      
+      
+      
+      res.json(resObj)
+      
+    })
 };
